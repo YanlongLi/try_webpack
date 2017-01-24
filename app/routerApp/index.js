4c7aabe6a37246218b125c94ce1c3ac2d1fea3a9
Yanlong Li <lansunlong@gmail.com>
2017-01-24 14:04:24 +0800

    try UiRoute, multi View

--- app/routerApp/index.js
+++ app/routerApp/index.js
@@ -1,27 +1,69 @@
-
 import angular from "angular";
 import $ from "jquery";
 import router from "angular-ui-router";
-console.log(router);
+import bootstrap from "bootstrap-webpack";
 
 var app = angular.module("routerApp", [router]);
-app.config(function($stateProvider, $urlRouterProvider){
-    $stateProvider.state("default",{
-        url: "/index",
-        template:"default page"
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("home");
+    $stateProvider.state("home", {
+        url: "/home",
+        templateUrl: "templates/home.html" // relative to root html page
+    }).state("home.list", {
+        url: "/list",
+        templateUrl: "templates/home-list.html",
+        controller: function($scope) {
+            $scope.dogs = ["Bernese", "Husky", "Goldendoodle"]
+        }
+    }).state("home.paragraph", {
+        url: "/paragraph",
+        template: "This is a paragraph"
     });
-    $stateProvider.state("name", {
-        url: "/name",
-        template:"hello ui-router {{id}}",
-        controller: ["$scope", function($scope){
-            console.log("asfa");
-            $scope.id = 1;
-        }]
+    $stateProvider.state("about", {
+        url: "/about",
+        views: {
+            "": {
+                templateUrl: "templates/about.html"
+            },
+            "columnOne@about": {
+                template: "<input type='text' ng-model='data.message'>",
+                controller: ["$scope", "DataSharing", function($scope, DataSharing){
+                    $scope.data = DataSharing;
+                }]
+            },
+            "columnTwo@about": {
+                templateUrl: "templates/about-data.html",
+                controller: "scotchController"
+            }
+        }
     });
-    $urlRouterProvider.otherwise("/index");
 });
 
-app.controller("TestCtl", function($scope){
-    $scope.name = 10;
-    console.log(10);
-});
+app.service("DataSharing", ["$http", function($http) {
+    this.message = 'test';
+    this.scotches = [{
+        name: 'Macallan 12',
+        price: 50
+    }, {
+        name: 'Chivas Regal Royal Salute',
+        price: 10000
+    }, {
+        name: 'Glenfiddich 1937',
+        price: 20000
+    }];
+}]);
+
+app.controller("scotchController", ["$scope", "DataSharing", function($scope, DataSharing) {
+    $scope.message = DataSharing.message;
+    $scope.scotches = DataSharing.scotches;
+    $scope.$watch(function() {
+        return DataSharing.message;
+    }, function(newVal, oldVal) {
+        $scope.message = DataSharing.message;
+    });
+    $scope.$watch(function() {
+        return DataSharing.scotches;
+    }, function() {
+        $scope.scotches = DataSharing.scotches;
+    });
+}]);
